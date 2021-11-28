361075facb3dc00e6889282f9fd3df170b502020
Vivekjothibai <vivek.jothibai@gmail.com>
2021-11-28 10:54:39 +0530

    first commit

---
+++ src/test/java/sample1/SampleTest1.java
@@ -0,0 +1,27 @@
+package sample1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src,new File("C:\\wrokspace\\vivek_workspace\\GitTest\\screenshot\\google.png"));
+		
+	}
+	
+}
